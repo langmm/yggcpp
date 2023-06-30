a7f92da066be357ddc2c219c1536301d3a1eaaf2
Meagan Lang <langmm.astro@gmail.com>
2023-06-30 13:25:58 -0400

    Add additional C interface tests Updated rapidjson

--- test/unittest/tests/communicators/interfacetest_c.cpp
+++ test/unittest/tests/communicators/interfacetest_c.cpp
@@ -63,3 +63,43 @@ INTERFACE_TEST_BASE(Base,
 		    INIT_DATA_CHAR, COMP_DATA_CHAR,
 		    ygg_send, (data_send, n_send), (data_send),
 		    ygg_recv, (data_recv, n_recv), (data_recv))
+
+INTERFACE_TEST_BASE(
+  Type,
+  INIT_INPUT_BASE(
+    yggInputType,
+    ("input", create_dtype_from_schema("{\"type\": \"number\"}", false)),
+    COMM_BASE,
+    ("", nullptr, SEND)); sComm.addSchema("{\"type\": \"number\"}"),
+  INIT_OUTPUT_BASE(
+    yggOutputType,
+    ("output", create_dtype_from_schema("{\"type\": \"number\"}", false)),
+    COMM_BASE,
+    ("", nullptr, RECV)),
+  INIT_DATA_SINGLE(double, 1.5), COMP_DATA_SINGLE,
+  yggSend, (data_send), (1, data_send),
+  yggRecv, (&data_recv), (1, &data_recv))
+
+INTERFACE_TEST_BASE(
+  Format,
+  INIT_INPUT_BASE(
+    yggInputFmt,
+    ("input", "%d\t%lf\t%5s"),
+    COMM_BASE,
+    ("", nullptr, SEND)); sComm.addFormat("%d\t%lf\t%5s"),
+  INIT_OUTPUT_BASE(
+    yggOutputFmt,
+    ("output", "%d\t%lf\t%5s"),
+    COMM_BASE,
+    ("", nullptr, RECV)),
+  INIT_DATA_TRIPLE, COMP_DATA_TRIPLE,
+  yggSend, (a_send, b_send, c_send, nc_send), SEND_NARGS_TRIPLE,
+  yggRecv, (&a_recv, &b_recv, &c_recv, &nc_recv), RECV_NARGS_TRIPLE)
+  
+INTERFACE_TEST_BASE(
+  AsciiTable,
+  INIT_INPUT_NOARGS(AsciiTable); sComm.addFormat("%d\t%lf\t%5s"),
+  INIT_OUTPUT(AsciiTable, "%d\t%lf\t%5s"),
+  INIT_DATA_TRIPLE, COMP_DATA_TRIPLE,
+  yggSend, (a_send, b_send, c_send, nc_send), SEND_NARGS_TRIPLE,
+  yggRecv, (&a_recv, &b_recv, &c_recv, &nc_recv), RECV_NARGS_TRIPLE)
