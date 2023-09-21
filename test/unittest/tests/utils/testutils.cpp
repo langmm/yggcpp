a5dbc5238f78263002ba46efaeefe0e48b2a0ad0
Meagan Lang <langmm.astro@gmail.com>
2023-09-21 17:18:33 -0400

    Re-enable more of address init

--- test/unittest/tests/utils/testutils.cpp
+++ test/unittest/tests/utils/testutils.cpp
@@ -42,17 +42,17 @@ TEST(Value, Complex) {
 
 TEST(ADDRESS, Init) {
     const std::string astr = "this.is.a.test";
-    // std::string cstr = "this.is.C.test";
+    std::string cstr = "this.is.C.test";
     auto *adr = new Address(astr);
     EXPECT_TRUE(adr->valid());
     EXPECT_EQ(adr->address(), astr);
     EXPECT_EQ(0, adr->key());
 
-    // Address adrc(cstr.c_str());
-    // EXPECT_TRUE(adrc.valid());
-    // EXPECT_EQ(adrc.address(), cstr);
-    // EXPECT_EQ(0, adrc.key());
-    // EXPECT_NE(adrc.address(), adr->address());
+    Address adrc(cstr.c_str());
+    EXPECT_TRUE(adrc.valid());
+    EXPECT_EQ(adrc.address(), cstr);
+    EXPECT_EQ(0, adrc.key());
+    EXPECT_NE(adrc.address(), adr->address());
 
     // auto* adrcmp = new Address(adr);
     // EXPECT_TRUE(adrcmp->valid());
