5d95384ee1d25bf217c54d5111852299ebfbf2d4
Meagan Lang <langmm.astro@gmail.com>
2023-10-09 15:57:55 -0500

    Add missing field to other type definition

--- python/pyYggCommBase.cpp
+++ python/pyYggCommBase.cpp
@@ -540,7 +540,10 @@ static PyTypeObject Comm_tType = {
         0,                         /* tp_version_tag */
         0,                         /* tp_finalize */
         0                          /* tp_vectorcall */
-
+#if (PY_MAJOR_VERSION > 3 || (PY_MAJOR_VERSION == 3 && PY_MINOR_VERSION >= 12))
+	,
+	0                          /* tp_watched */
+#endif
 };
 
 
