a72b5192e43b5b1e5c46f7ee9ab5161b2de88eee
Meagan Lang <langmm.astro@gmail.com>
2023-11-02 12:52:48 -0400

    Disable redefinition

--- communication/communicators/CommBase.hpp
+++ communication/communicators/CommBase.hpp
@@ -1,8 +1,8 @@
 #pragma once
 #define VIRT_END = 0
-#if defined(_WINDOWS) && !(defined(YggInterface_EXPORTS) || defined(YggInterface_py_EXPORTS) || defined(RAPIDJSON_FORCE_IMPORT_ARRAY))
-extern __declspec(dllimport) void** rapidjson_ARRAY_API;
-#endif
+// #if defined(_WINDOWS) && !(defined(YggInterface_EXPORTS) || defined(YggInterface_py_EXPORTS) || defined(RAPIDJSON_FORCE_IMPORT_ARRAY))
+// extern __declspec(dllimport) void** rapidjson_ARRAY_API;
+// #endif
 #include "utils/tools.hpp"
 #include "utils/enums.hpp"
 #include "utils/Address.hpp"
