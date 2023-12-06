23d09159dbe9fc66bc8dbfb9116fae0196bb1bae
Meagan Lang <langmm.astro@gmail.com>
2023-12-06 14:22:17 -0600

    Fix bugs with refactor of cmake for install Move python tests into cmake Clean up debug messages in cmake Migrate speed tests into run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,33 +1,62 @@
 set -e
 
+LANGUAGE=""
+REBUILD=""
+DONT_BUILD=""
+DO_C=""
+DO_CXX=""
 DO_FORTRAN=""
 DO_PYTHON=""
 WITH_ASAN=""
-DONT_BUILD=""
 NO_CORE=""
-CMAKE_FLAGS="-DRAPIDJSON_INCLUDE_DIRS=/Users/langmm/rapidjson/include"
+CMAKE_FLAGS="-DRAPIDJSON_INCLUDE_DIRS=/Users/langmm/rapidjson/include -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DYGG_SKIP_VALGRIND_TESTS=ON"
+CMAKE_FLAGS_LIB=""
+CMAKE_FLAGS_SPEED=""
 WITH_LLDB=""
 DO_SYMBOLS=""
 DONT_TEST=""
 NO_DEBUG_MSG=""
+SPEED_TEST=""
+N_MSG="100"
+S_MSG="100"
+COMM="DEFAULT_COMM"
 
 while [[ $# -gt 0 ]]; do
     case $1 in
+	-c )
+	    DO_C="TRUE"
+	    shift # past argument with no value
+	    ;;
+	--cxx )
+	    DO_CXX="TRUE"
+	    shift # past argument with no value
+	    ;;
 	-p | --python )
 	    DO_PYTHON="TRUE"
+	    CMAKE_FLAGS="${CMAKE_FLAGS} -DBUILD_PYTHON_LIBRARY=ON -DYGG_BUILD_PYTHON_TESTS=ON"
 	    shift # past argument with no value
 	    ;;
 	-f | --fortran )
 	    DO_FORTRAN="TRUE"
+	    CMAKE_FLAGS="${CMAKE_FLAGS} -DBUILD_FORTRAN_LIBRARY=ON -DYGG_BUILD_FORTRAN_TESTS=ON -DYGG_ENABLE_ELF=OFF"
+	    shift # past argument with no value
+	    ;;
+	-l | --language )
+	    LANGUAGE="$2"
+	    shift
+	    shift
+	    ;;
+	--rebuild )
+	    REBUILD="TRUE"
+	    shift
+	    ;;
+	--dont-build )
+	    DONT_BUILD="TRUE"
 	    shift # past argument with no value
 	    ;;
 	--with-asan )
 	    WITH_ASAN="TRUE"
 	    CMAKE_FLAGS="${CMAKE_FLAGS} -DYGG_BUILD_ASAN=ON -DYGG_BUILD_UBSAN=ON"
-	    shift # past argument with no value
-	    ;;
-	--dont-build )
-	    DONT_BUILD="TRUE"
 	    shift # past argument with no value
 	    ;;
 	--without-core )
@@ -72,47 +101,118 @@ while [[ $# -gt 0 ]]; do
 	    NO_DEBUG_MSG="TRUE"
 	    shift # past argument with no value
 	    ;;
+	--speed )
+	    SPEED_TEST="TRUE"
+	    shift # past argument with no value
+	    ;;
+	-n | --n-msg )
+	    N_MSG="$2"
+	    shift
+	    shift
+	    ;;
+	-s | --s-msg )
+	    S_MSG="$2"
+	    shift
+	    shift
+	    ;;
+	--comm )
+	    COMM="$2"
+	    shift
+	    shift
+	    ;;
 	*)
 	    ;;
     esac
 done
 
+if [ -n "$SPEED_TEST" ]; then
+    NO_DEBUG_MSG="TRUE"
+    DONT_TEST="TRUE"
+fi
+
+if [ "$LANGUAGE" = "C" ]; then
+    DO_C="TRUE"
+elif [ "$LANGUAGE" = "CXX" ]; then
+    DO_CXX="TRUE"
+elif [ "$LANGUAGE" = "FORTRAN" ]; then
+    DO_FORTRAN="TRUE"
+elif [ "$LANGUAGE" = "PYTHON" ]; then
+    DO_PYTHON="TRUE"
+fi
+
+if [ ! -n "$DO_C" ] && [ ! -n "$DO_CXX" ] && [ ! -n "$DO_FORTRAN" ] && [ ! -n "$DO_PYTHON" ]; then
+    DO_C="TRUE"
+    DO_CXX="TRUE"
+    DO_FORTRAN="TRUE"
+    DO_PYTHON="TRUE"
+    CMAKE_FLAGS_LIB="${CMAKE_FLAGS_LIB} -DYGG_BUILD_TESTS=ON"
+fi
+
+if [ -n "$DO_C" ] || [ -n "$DO_CXX" ] || [ -n "$DO_FORTRAN" ] || [ -n "$DO_PYTHON" ]; then
+    if [ -n "$DO_C" ]; then
+	CMAKE_FLAGS_SPEED="${CMAKE_FLAGS_SPEED} -DENABLE_C=ON"
+    else
+	CMAKE_FLAGS_SPEED="${CMAKE_FLAGS_SPEED} -DENABLE_C=OFF"
+    fi
+    if [ -n "$DO_CXX" ]; then
+	CMAKE_FLAGS_SPEED="${CMAKE_FLAGS_SPEED} -DENABLE_CXX=ON"
+    else
+	CMAKE_FLAGS_SPEED="${CMAKE_FLAGS_SPEED} -DENABLE_CXX=OFF"
+    fi
+    if [ -n "$DO_C" ] || [ -n "$DO_CXX" ]; then
+	CMAKE_FLAGS_LIB="${CMAKE_FLAGS_LIB} -DBUILD_CPP_LIBRARY=ON -DYGG_BUILD_CPP_TESTS=ON"
+    else
+	CMAKE_FLAGS_LIB="${CMAKE_FLAGS_LIB} -DBUILD_CPP_LIBRARY=OFF"
+    fi
+    if [ -n "$DO_FORTRAN" ]; then
+	CMAKE_FLAGS_LIB="${CMAKE_FLAGS_LIB} -DBUILD_FORTRAN_LIBRARY=ON -DYGG_BUILD_FORTRAN_TESTS=ON"
+	CMAKE_FLAGS_SPEED="${CMAKE_FLAGS_SPEED} -DENABLE_FORTRAN=ON"
+    else
+	CMAKE_FLAGS_SPEED="${CMAKE_FLAGS_SPEED} -DENABLE_FORTRAN=OFF"
+    fi
+    if [ -n "$DO_PYTHON" ]; then
+	CMAKE_FLAGS_LIB="${CMAKE_FLAGS_LIB} -DBUILD_PYTHON_LIBRARY=ON -DYGG_BUILD_PYTHON_TESTS=ON"
+	CMAKE_FLAGS_SPEED="${CMAKE_FLAGS_SPEED} -DENABLE_PYTHON=ON"
+    else
+	CMAKE_FLAGS_SPEED="${CMAKE_FLAGS_SPEED} -DENABLE_PYTHON=OFF"
+    fi
+fi
+	
 if [ ! -n "$NO_DEBUG_MSG" ]; then
     CMAKE_FLAGS="${CMAKE_FLAGS} -DYGG_DEBUG_LEVEL=5"
 fi
 
-if [ -n "$DO_PYTHON" ]; then
+if [ -n "$WITH_ASAN" ]; then
+    export ASAN_OPTIONS=symbolize=1
+    export ASAN_SYMBOLIZER_PATH=$(which llvm-symbolizer)
+fi
+
+if [ -n "$REBUILD" ]; then
+    DONT_BUILD=""
+    if [ -d "build" ]; then
+	rm -rf "build"
+    fi
+    if [ -d "_skbuild" ]; then
+	rm -rf "_skbuild"
+    fi
+    if [ -f "communication/pyYggdrasil/pyYggdrasil.cpython-39-darwin.so" ]; then
+	rm "communication/pyYggdrasil/pyYggdrasil.cpython-39-darwin.so"
+    fi
+    if [ -f "communication/pyYggdrasil/lib/libYggInterface_py.dylib" ]; then
+	rm "communication/pyYggdrasil/lib/libYggInterface_py.dylib"
+    fi
+fi
+export CMAKE_ARGS=${CMAKE_FLAGS}
+if [ -n "$DO_PYTHON" ] && [ -n "$NO_CORE" ]; then
+    export CMAKE_ARGS="${CMAKE_FLAGS} ${CMAKE_FLAGS_LIB}"
     if [ ! -n "$DONT_BUILD" ]; then
-	if [ -d "_skbuild" ]; then
-	    rm -rf "_skbuild"
-	fi
-	if [ -f "communication/pyYggdrasil/pyYggdrasil.cpython-39-darwin.so" ]; then
-	    rm "communication/pyYggdrasil/pyYggdrasil.cpython-39-darwin.so"
-	fi
-	if [ -f "communication/pyYggdrasil/lib/libYggInterface_py.dylib" ]; then
-	    rm "communication/pyYggdrasil/lib/libYggInterface_py.dylib"
-	fi
-	
-	if [ -n "$WITH_ASAN" ]; then
-	    export ASAN_OPTIONS=symbolize=1
-	    export ASAN_SYMBOLIZER_PATH=$(which llvm-symbolizer)
-	fi
-	export CMAKE_ARGS=${CMAKE_FLAGS}
-	if [ -n "$NO_CORE" ]; then
-	    python3 setup.py build_ext --inplace
-	else
-	    pip3 install . -v
-	fi
+	python3 setup.py build_ext --inplace
     fi
     if [ -n "$WITH_ASAN" ]; then
 	export DYLD_INSERT_LIBRARIES=$(clang -print-file-name=libclang_rt.asan_osx_dynamic.dylib)
     fi
-    if [ -n "$NO_CORE" ]; then
-	cd python/pyYggdrasil
-	export TEST_DIR=../test
-    else
-	export TEST_DIR=python/test
-    fi
+    cd python/pyYggdrasil
+    export TEST_DIR=../test
     if [ ! -n "$DONT_TEST" ]; then
 	export PYTHONFAULTHANDLER=1
 	if [ -n "$WITH_LLDB" ]; then
@@ -121,9 +221,7 @@ if [ -n "$DO_PYTHON" ]; then
 	    python3 -m pytest -svx $TEST_DIR
 	fi
     fi
-    if [ -n "$NO_CORE" ]; then
-	cd ../../
-    fi
+    cd ../../
 else
     DYLD_PRINT_LIBRARIES=1
     DYLD_PRINT_APIS=1
@@ -134,17 +232,10 @@ else
     fi
     cd build
     if [ ! -n "$DONT_BUILD" ]; then
-	if [ -n "$DO_FORTRAN" ]; then
-	    # unset CFLAGS
-	    # unset CXXFLAGS
-	    # unset FFLAGS
-	    # CMAKE_FLAGS="${CMAKE_FLAGS} -DCMAKE_C_COMPILER=gcc-13 -DCMAKE_CXX_COMPILER=g++-13 -DCMAKE_Fortran_COMPILER=gfortran-13"
-	    CMAKE_FLAGS="${CMAKE_FLAGS} -DBUILD_FORTRAN_LIBRARY=ON -DYGG_BUILD_FORTRAN_TESTS=ON -DYGG_ENABLE_ELF=OFF"
-	else
-	    CMAKE_FLAGS="${CMAKE_FLAGS} -DYGG_BUILD_CXX_TESTS=ON"
-	fi
-	cmake .. -DCMAKE_INSTALL_PREFIX=../devel -DYGG_ENABLE_COVERAGE=OFF -DYGG_SKIP_VALGRIND_TESTS=ON -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DBUILD_PYTHON_LIBRARY=OFF $CMAKE_FLAGS
+	cmake .. $CMAKE_FLAGS $CMAKE_FLAGS_LIB
 	make
+	# cmake --build .
+	# cmake --install . --prefix ../devel
     fi
     if [ ! -n "$DONT_TEST" ]; then
 	if [ -n "$WITH_LLDB" ]; then
@@ -157,7 +248,27 @@ else
 	    make test ARGS="--stop-on-failure"
 	fi
     fi
-    cd ../
+    cd ..
+fi
+
+if [ -n "$SPEED_TEST" ]; then
+    if [ -n "$REBUILD" ]; then
+	if [ -d "build_speed" ]; then
+	    rm -rf build_speed
+	fi
+    fi
+    if [ ! -d "build_speed" ]; then
+	mkdir build_speed
+    fi
+    cd build_speed
+    cmake ../test/speedtest -DCMAKE_PREFIX_PATH=../devel -DYggInterface_DIR=../../../devel/lib/cmake/YggInterface -DN_MSG=$N_MSG -DS_MSG=$S_MSG -DCOMM=$COMM $CMAKE_FLAGS $CMAKE_FLAGS_SPEED
+    cmake --build .
+    # cmake --install . --prefix .
+    if [ -n "$WITH_ASAN" ]; then
+	export DYLD_INSERT_LIBRARIES=$(clang -print-file-name=libclang_rt.asan_osx_dynamic.dylib)
+    fi
+    make test
+    cd ..
 fi
 
 if [ -n "$DO_SYMBOLS" ]; then
