e1bf9834e09a31112621e8feeedc2aace5e71843
Meagan Lang <langmm.astro@gmail.com>
2024-02-07 11:35:57 -0500

    Fix syntax for python classes due to extra/missing comma

--- python/communicators.cpp
+++ python/communicators.cpp
@@ -170,12 +170,10 @@ static PyTypeObject commMetaType = {
         0,                         /* tp_version_tag */
         0,                         /* tp_finalize */
 #if (PY_MAJOR_VERSION > 3 || (PY_MAJOR_VERSION == 3 && PY_MINOR_VERSION >= 8))
-	,
-        0                          /* tp_vectorcall */
+        0,                         /* tp_vectorcall */
 #endif
 #if (PY_MAJOR_VERSION > 3 || (PY_MAJOR_VERSION == 3 && PY_MINOR_VERSION >= 12))
-	,
-	0                          /* tp_watched */
+	0,                         /* tp_watched */
 #endif
 };
 
@@ -614,11 +612,10 @@ static PyTypeObject Comm_tType = {
         0,                         /* tp_version_tag */
         0,                         /* tp_finalize */
 #if (PY_MAJOR_VERSION > 3 || (PY_MAJOR_VERSION == 3 && PY_MINOR_VERSION >= 8))
-        0                          /* tp_vectorcall */
+        0,                         /* tp_vectorcall */
 #endif
 #if (PY_MAJOR_VERSION > 3 || (PY_MAJOR_VERSION == 3 && PY_MINOR_VERSION >= 12))
-	,
-	0                          /* tp_watched */
+	0,                         /* tp_watched */
 #endif
 };
 
