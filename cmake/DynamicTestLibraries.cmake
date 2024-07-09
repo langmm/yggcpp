3e60f9d2eaa1208eed03b29b24de730dd7d35e98
Meagan Lang <langmm.astro@gmail.com>
2024-07-08 21:04:28 -0400

    Standardize cmake function for adding dynamic library example

--- cmake/DynamicTestLibraries.cmake
+++ cmake/DynamicTestLibraries.cmake
@@ -1,3 +1,43 @@
+function(add_dynamic_test_library TARGET)
+  set(oneValueArgs LANGUAGE)
+  set(multiValueArgs SOURCES LIBRARIES INCLUDES)
+  cmake_parse_arguments(ARGS "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+  message(STATUS "ARGS_SOURCES = ${ARGS_SOURCES}")
+  message(STATUS "ARGS_LANGUAGE = ${ARGS_LANGUAGE}")
+  if (ARGS_SOURCES)
+    set(sources ${ARGS_SOURCES})
+  else()
+    set(sources ${ARGS_UNPARSED_ARGUMENTS})
+  endif()
+  if ((ARGS_LANGUAGE STREQUAL "Fortran") AND (FORCE_SPLIT_CXXFORTRAN OR MSVC))
+    include(YggAddFortranSubdirectory)
+    add_mixed_fortran_library(
+      ${TARGET} SHARED LANGUAGE CXX
+      SOURCES ${sources}
+      LIBRARIES ${ARGS_LIBRARIES}
+      INCLUDES ${ARGS_INCLUDES}
+    )
+  else()
+    add_library(${TARGET} SHARED ${sources})
+    if (ARGS_LANGUAGE STREQUAL "Fortran")
+      set_target_properties(${TARGET} PROPERTIES LINKER_LANGUAGE CXX)
+    endif()
+    if (ARGS_LIBRARIES)
+      target_link_libraries(${TARGET} PUBLIC ${ARGS_LIBRARIES})
+    endif()
+    if (ARGS_INCLUDES)
+      target_include_directories(${TARGET} PUBLIC ${ARGS_INCLUDES})
+    endif()
+  endif()
+  if (WIN32)
+    set_target_properties(${TARGET} PROPERTIES WINDOWS_EXPORT_ALL_SYMBOLS ON)
+  endif()
+  # TODO: Output directory to file that can be loaded
+  include(CheckDLL)
+  show_symbols(${TARGET})
+endfunction()
+
+
 function(add_dynamic_dependencies TARGET)
   add_dependencies(${TARGET} ${ARGN})
   cmake_path(GET CMAKE_CURRENT_BINARY_DIR PARENT_PATH PARENT_BINARY_DIR)
@@ -7,6 +47,7 @@ function(add_dynamic_dependencies TARGET)
       POST_BUILD
       COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:${lib}> ${PARENT_BINARY_DIR}
       COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:${lib}> ${CMAKE_CURRENT_BINARY_DIR}
+      COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:${lib}> $<TARGET_FILE_DIR:${TARGET}>
       COMMAND_EXPAND_LISTS
     )
   endforeach()
