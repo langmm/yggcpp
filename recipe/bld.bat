76c028bf41b79174d0f12f138962a79103f2c204
Meagan Lang <langmm.astro@gmail.com>
2025-10-08 12:52:41 -0400

    Enable build for C & CXX libraries in conda recipe

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,20 +4,20 @@ set "SKBUILD_CMAKE_ARGS=-G Ninja"
 %PYTHON% -m pip install . --no-deps --ignore-installed -vvv --no-build-isolation
 if errorlevel 1 exit 1
 
-rem mkdir conda_build
-rem cd conda_build
+mkdir conda_build
+cd conda_build
 
-rem cmake %CMAKE_ARGS% ^
-rem       -G "Ninja" ^
-rem       -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON ^
-rem       -D "Python3_EXECUTABLE:FILEPATH=%PYTHON%" ^
-rem       -D BUILD_CPP_LIBRARY:BOOL=ON ^
-rem       -D BUILD_PYTHON_LIBRARY:BOOL=ON ^
-rem       -D YGG_Fortran_REQUIRED:BOOL=ON ^
-rem       ..
-rem if errorlevel 1 exit 1
+cmake %CMAKE_ARGS% ^
+      -G "Ninja" ^
+      -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON ^
+      -D "Python3_EXECUTABLE:FILEPATH=%PYTHON%" ^
+      -D BUILD_CPP_LIBRARY:BOOL=ON ^
+      -D BUILD_PYTHON_LIBRARY:BOOL=OFF ^
+      -D BUILD_FORTRAN_LIBRARY:BOOL=OFF ^
+      ..
+if errorlevel 1 exit 1
 
-rem cmake --install .
-rem if errorlevel 1 exit 1
+cmake --install .
+if errorlevel 1 exit 1
 
 @endlocal
--- recipe/build.sh
+++ recipe/build.sh
@@ -5,19 +5,21 @@ set -euo pipefail
 CMAKE_GENERATOR="Ninja" $PYTHON -m pip install . --no-deps --ignore-installed -vvv --no-build-isolation
 
 # Build C/CXX & Fortran libraries using cmake
-# if [ ! -d conda_build ]; then
-#     mkdir conda_build
-# fi
-# cd conda_build
-# cmake ${CMAKE_ARGS} \
-#       -G "Ninja" \
-#       -D Python3_EXECUTABLE=$PYTHON \
-#       -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON \
-#       -D BUILD_CPP_LIBRARY:BOOL=ON \
-#       -D BUILD_PYTHON_LIBRARY:BOOL=ON \
-#       -D YGG_Fortran_REQUIRED:BOOL=ON \
-#       ..
+if [ ! -d conda_build ]; then
+    mkdir conda_build
+fi
+cd conda_build
+cmake ${CMAKE_ARGS} \
+      -G "Ninja" \
+      -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+      -D Python3_EXECUTABLE=$PYTHON \
+      -D BUILD_CPP_LIBRARY:BOOL=ON \
+      -D BUILD_PYTHON_LIBRARY:BOOL=OFF \
+      -D BUILD_FORTRAN_LIBRARY:BOOL=OFF \
+      ..
 
-# cmake --install .
+# -D YGG_Fortran_REQUIRED:BOOL=ON
 
-# cd ..
+cmake --install .
+
+cd ..
