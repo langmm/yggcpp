6afc111c95bd528568fef3589636d28ae84a8973
Meagan Lang <langmm.astro@gmail.com>
2026-06-09 16:58:16 -0400

    Fix syntax in script to run speedtests for windows conda recipe build

--- conda.recipe/tests/run_test.bat
+++ conda.recipe/tests/run_test.bat
@@ -1,13 +1,12 @@
 setlocal EnableDelayedExpansion
 
-set "speeddir=tests\\speedtest"
 set "builddir=build_speed"
 if not exist "!builddir!" mkdir "!builddir!"
 if !errorlevel! neq 0 exit /b !errorlevel!
 cd "!builddir!"
 cmake -G "Ninja" ^
       -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON ^
-      "!speeddir!"
+      ..\\tests\\speedtest
 if !errorlevel! neq 0 exit /b !errorlevel!
 cmake --build . --config Release
 if !errorlevel! neq 0 exit /b !errorlevel!
