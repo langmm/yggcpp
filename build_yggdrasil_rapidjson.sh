997e5931a4bd8e641ff7c6bcfa0de5f0de122bff
Meagan Lang <langmm.astro@gmail.com>
2026-06-11 09:39:23 -0400

    Update build_yggdrasil-rapidjson.sh to use specific directory for checkout/build/install and set Python paths Add enabling of CXX for linking to YggInterfaceConfig Update speedtest to remove explicit setting of linker language

--- build_yggdrasil_rapidjson.sh
+++ build_yggdrasil_rapidjson.sh
@@ -1,26 +1,46 @@
 #!/bin/bash
 set -e
 
-INSTALL_DIR="$(pwd)/_install"
+RJ_DIR=""
+BUILD_DIR=""
+INSTALL_DIR=""
 while [[ $# -gt 0 ]]; do
     case $1 in
+        --rj-dir )
+            RJ_DIR="$2"
+	    shift
+	    shift # past argument with value
+	    ;;
+        --build-dir )
+            BUILD_DIR="$2"
+	    shift
+	    shift # past argument with value
+	    ;;
         --install-dir )
             INSTALL_DIR="$2"
-            shift
-            shift
-            ;;
-        *)
-            shift
-            ;;
+	    shift
+	    shift # past argument with value
+	    ;;
     esac
 done
-
-if [ ! -d yggdrasil-rapidjson ]; then
-    git clone https://github.com/cropsinsilico/yggdrasil-rapidjson.git
+if [ ! -n "${RJ_DIR}" ]; then
+    RJ_DIR="$(pwd)/yggdrasil-rapidjson"
 fi
-cd yggdrasil-rapidjson
-if [ ! -d build ]; then
-    mkdir build
+if [ ! -n "${BUILD_DIR}" ]; then
+    BUILD_DIR="${RJ_DIR}/build"
+fi
+if [ ! -n "${INSTALL_DIR}" ]; then
+    INSTALL_DIR="${RJ_DIR}/_install"
+fi
+
+if [ ! -d ${RJ_DIR} ]; then
+    git clone --branch yggdrasil --recurse-submodules https://github.com/cropsinsilico/yggdrasil-rapidjson.git ${RJ_DIR}
+fi
+if [ ! -d ${BUILD_DIR} ]; then
+    mkdir ${BUILD_DIR}
+fi
+if [ ! -d ${INSTALL_DIR} ]; then
+    mkdir ${INSTALL_DIR}
 fi
 
 PYTHON=${PYTHON:-$(which python)}
@@ -32,13 +52,15 @@ CMAKE_ARGS+=" -DPython3_INCLUDE_DIR:PATH=${Python_INCLUDE_DIR}"
 CMAKE_ARGS+=" -DPython3_NumPy_INCLUDE_DIR=${Python_NumPy_INCLUDE_DIR}"
 CMAKE_ARGS+=" -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}"
 
-
-cmake -B build -S . \
-      -G "Ninja" \
-      -D YGGDRASIL_RAPIDJSON_BUILD_TESTS:BOOL=OFF \
-      -D YGGDRASIL_RAPIDJSON_BUILD_EXAMPLES:BOOL=OFF \
-      -D YGGDRASIL_RAPIDJSON_BUILD_DOC:BOOL=OFF \
+cmake -B ${BUILD_DIR} -S ${RJ_DIR} \
+      -G Ninja \
       -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+      -D YGGDRASIL_RAPIDJSON_BUILD_EXAMPLES:BOOL=OFF \
+      -D YGGDRASIL_RAPIDJSON_BUILD_TESTS:BOOL=OFF \
+      -D YGGDRASIL_RAPIDJSON_BUILD_DOC:BOOL=OFF \
       ${CMAKE_ARGS}
-cmake --build build
-cmake --install build --prefix "$INSTALL_DIR"
+cmake --build ${BUILD_DIR}
+cmake --install ${BUILD_DIR} --prefix "${INSTALL_DIR}"
+      
+
+
